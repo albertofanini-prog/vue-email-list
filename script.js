e563abf234e57a40bb4d5218c81b96d5a5235a1c
albertofanini-prog <albertofanini@outlook.com>
2022-03-04 14:55:00 +0100

    Ricevere una mail da API in console

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ const app = new Vue({
     }
 
 })
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(function (response){
+        const result = response.data;
+        console.log(result);
+    })
