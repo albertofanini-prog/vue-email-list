a98c76cf6f346f599219e46f448fa93afe0b8523
albertofanini-prog <albertofanini@outlook.com>
2022-03-04 15:53:09 +0100

    Stampa in html

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ console.log('Hello Vue email list by API.');
 const app = new Vue({
     el: '#app',
     data:{
-        email:'boolean_class-54@outlook.com'
-    }
+        //email:'boolean_class-54@outlook.com',
+        emailList:[],
+    },
+    created(){
+        for (let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (res) => {
 
+                this.emailList.push(res.data.response);
+            })
+        }
+    }
 })
 
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(function (response){
-        const result = response.data;
-        console.log(result);
-    })
+console.log(app);
+
